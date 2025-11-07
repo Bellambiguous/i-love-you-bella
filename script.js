dee9782efd5fa4a6465fc52d11578acd7e7c7968
Bellambiguous <tamiltopia@gmail.com>
2025-11-07 04:52:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const candle = document.getElementById('candle');
+let pos = { x: window.innerWidth/2, y: window.innerHeight/2 };
+let mouse = { x: pos.x, y: pos.y };
 
 document.addEventListener('mousemove', e => {
-    const x = e.clientX - candle.offsetWidth/2;
-    const y = e.clientY - candle.offsetHeight/2;
-
-    // Smooth movement
-    candle.style.left = x + 'px';
-    candle.style.top = y + 'px';
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
 });
+
+function animate() {
+    pos.x += (mouse.x - pos.x) * 0.1; // smooth easing
+    pos.y += (mouse.y - pos.y) * 0.1;
+
+    candle.style.left = (pos.x - candle.offsetWidth/2) + 'px';
+    candle.style.top = (pos.y - candle.offsetHeight/2) + 'px';
+
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+
